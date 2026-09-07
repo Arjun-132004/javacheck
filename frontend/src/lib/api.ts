133f42128e98b0813399ce9da03d546b258ba8cb
Arjun T M <arjuntm@tecneural.com>
2026-09-07 19:34:56 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,7 @@
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 const API_URL = import.meta.env.VITE_API_URL;
 
+// this is the aoi call 
 export const endpoints = {
 	auth: {
 		loginWithProvider: (provider: string) =>
